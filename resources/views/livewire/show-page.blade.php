45a9deb3c83546e2136d8b4c0719ca3f6d730067
Oliver Jimenez-Servin <oliverds@users.noreply.github.com>
2021-03-10 17:02:55 -0600

    Fix edit page link

--- resources/views/livewire/show-page.blade.php
+++ resources/views/livewire/show-page.blade.php
@@ -39,7 +39,7 @@
                 <div class="mt-3 sm:mt-0 sm:ml-4">
                     <!-- Settings Dropdown -->
                     <div class="flex items-center space-x-4">
-                        <a class="text-gray-500 hover:text-gray-700 transition duration-150 ease-in-out" href="{{ route('contentful.posts.edit', $post) }}">
+                        <a class="text-gray-500 hover:text-gray-700 transition duration-150 ease-in-out" href="{{ route('contentful.pages.edit', $post) }}">
                             <svg class="fill-current h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                 <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
                             </svg>
