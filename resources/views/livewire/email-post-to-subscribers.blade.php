48fd132495d7ce142b1e63e29a70367e676d7ce2
Oliver Jimenez-Servin <oliverds@users.noreply.github.com>
2021-05-09 18:15:32 -0500

    wip

--- resources/views/livewire/email-post-to-subscribers.blade.php
+++ resources/views/livewire/email-post-to-subscribers.blade.php
@@ -3,7 +3,7 @@
         <div class="pb-6">
             <div class="bg-gray-50 border border-gray-200 sm:rounded-lg">
                 <div class="px-4 py-5 sm:p-6">
-                    <p class="text-center">{{ __('If you'd also like to email this post to your subscribers, click the button below.') }}</p>
+                    <p class="text-center">{{ __('If you’d also like to email this post to your subscribers, click the button below.') }}</p>
 
                     <div class="flex justify-center space-x-2 mt-5">
                         <x-wordful::button.primary wire:click="emailPost">
